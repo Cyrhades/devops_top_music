3629dd222654ec8ee20e920d0f3de661fee13f95
cyrhades <cyrhades76@gmail.com>
2024-01-22 11:56:34 +0100

    refactorisation est correction d'un async await manquant

---
+++ src/controllers/Search.js
@@ -0,0 +1,29 @@
+const apiDeezer = require("../services/deezer.js");
+const apiTopMusic = require("../services/topMusic.js")
+
+exports.get = (req, res) => {
+    const api = new apiDeezer();
+    const topMusic = new apiTopMusic();
+    if(req.query.keyword != undefined && req.query.keyword.trim() != '') {
+        api.search(req.query.keyword).then(async (result) => {
+            let musics = [];
+            let promises = [];   
+            for(element of result.data) {
+                promises[promises.length] = await topMusic.getCover(element.album).then(cover => {
+                    musics.push({
+                        title: element.title,
+                        preview: element.preview,
+                        cover
+                    });
+                })
+            }
+            Promise.all(promises).then(() => {
+                res.render('home', {keyword: req.query.keyword, musics});           
+            })
+        }).catch((result) => {
+            console.log(result)
+        });  
+    } else {
+        res.render('home')
+    }
+} 
